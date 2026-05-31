dcc76160a7fa30404e6c873f26314756c0b982c1
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-05-31 12:24:59 -0700

    Move preloading to initialize scene

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -3,31 +3,6 @@ class Play extends Phaser.Scene {
     constructor() {
         super('playScene');
     }
- 
-    preload() {
-
-        this.load.setPath("./Assets");
-        this.load.image('photo', 'family-photo.png');
-
-        this.load.setPath("./Assets/Faces");
-        this.load.image('angry', 'face-angry.png');
-        this.load.image('blink', 'face-blink.png');
-        this.load.image('happy', 'face-happy.png');
-        this.load.image('neutral', 'face-neutral.png');
-        this.load.image('sad', 'face-sad.png');
-
-        this.load.setPath("./Assets/CheckAndXMarks");
-        this.load.image('checkMark', 'CheckMark.png');
-        this.load.image('greenBox', 'GreenBox.png');
-        this.load.image('xMark', 'XMark.png');
-        this.load.image('redBox', 'RedBox.png');
-
-        this.load.setPath("./Assets/Sounds")
-        this.load.audio('correct', 'CorrectSound.wav');
-        this.load.audio('incorrect', 'IncorrectSound.wav');
-        this.load.audio('cameraClick', 'Camera.wav');
-
-    }
 
     create() {
 

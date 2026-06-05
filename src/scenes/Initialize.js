667017c09a52c209af545f38016ea080b5acd2a3
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-06-05 09:48:40 -0700

    Quotes

--- src/scenes/Initialize.js
+++ src/scenes/Initialize.js
@@ -7,27 +7,27 @@ class Initialize extends Phaser.Scene {
  
     preload() {
 
-        this.load.setPath("./Assets");
+        this.load.setPath('./Assets');
         this.load.image('background', 'background.png');
         this.load.image('photo', 'family-photo.png');
 
-        this.load.setPath("./Assets/Camera");
+        this.load.setPath('./Assets/Camera');
         this.load.image('cameraOverlay', 'CameraOverlay.png');
 
-        this.load.setPath("./Assets/Faces");
+        this.load.setPath('./Assets/Faces');
         this.load.image('angry', 'face-angry.png');
         this.load.image('blink', 'face-blink.png');
         this.load.image('happy', 'face-happy.png');
         this.load.image('neutral', 'face-neutral.png');
         this.load.image('sad', 'face-sad.png');
 
-        this.load.setPath("./Assets/CheckAndXMarks");
+        this.load.setPath('./Assets/CheckAndXMarks');
         this.load.image('checkMark', 'CheckMark.png');
         this.load.image('greenBox', 'GreenBox.png');
         this.load.image('xMark', 'XMark.png');
         this.load.image('redBox', 'RedBox.png');
 
-        this.load.setPath("./Assets/Sounds");
+        this.load.setPath('./Assets/Sounds');
         this.load.audio('correct', 'CorrectSound.wav');
         this.load.audio('incorrect', 'IncorrectSound.wav');
         this.load.audio('cameraClick', 'Camera.wav');
@@ -35,7 +35,7 @@ class Initialize extends Phaser.Scene {
         this.load.audio('cheese', 'Cheese.m4a');
         this.load.audio('backgroundMusic', 'BackgroundMusic.mp3');
 
-        this.load.setPath("./Assets/Fonts");
+        this.load.setPath('./Assets/Fonts');
         this.load.font('Vito_bold', 'VitoBold.otf', 'opentype');        
 
     }
